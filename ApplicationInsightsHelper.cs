caa9e042168a1fa4206444b1a34d44c0d6b42029
pika4tw <pika4tw@hotmail.com>
2018-10-11 15:09:39 +0800

    Add files via upload

---
+++ ApplicationInsightsHelper.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Windows.Networking.Connectivity;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+
+namespace IntelligentKioskSample
+{
+    class ApplicationInsightsHelper
+    {
+
+        private static TelemetryClient context = null;
+        private static string InstrumentationKey = SettingsHelper.Instance.ApplicationInsightsKey;
+        private static bool enable = false;
+        public static void Initialize()
+        {
+            if (InstrumentationKey.Length == 0)
+            {
+                return;
+            }
+
+            TelemetryClient tc = new TelemetryClient();
+            tc.InstrumentationKey = InstrumentationKey;
+            EasClientDeviceInformation deviceInfo = new EasClientDeviceInformation();
+
+            // Set session data:
+            tc.Context.User.Id = deviceInfo.FriendlyName;
+            tc.Context.Session.Id = Guid.NewGuid().ToString();
+            tc.Context.Device.OperatingSystem = deviceInfo.FriendlyName;
+
+            context = tc;
+            enable = true;
+        }
+
+        public static void TrackPage(string name)
+        {
+            if (context == null) { Initialize(); }
+            if (!enable) { return; }
+            PageViewTelemetry page = new PageViewTelemetry(name);
+            page.Context.Operation.Name = name;
+            context.TrackPageView(page);
+        }
+
+
+        public static void TrackException(Exception e)
+        {
+            if (context == null) { Initialize(); }
+            if (!enable) { return; }
+            ExceptionTelemetry exception = new ExceptionTelemetry(e);
+            exception.Context.Operation.Name = "Exception";
+            context.TrackException(exception);
+        }
+
+        public static void TrackEvent(string ename, Dictionary<String, String> dictionary)
+        {
+            if (context == null) { Initialize(); }
+            if (!enable) { return; }
+
+            EventTelemetry e = new EventTelemetry();
+            e.Name = ename;
+            foreach (KeyValuePair<string, string> item in dictionary)
+            {
+                e.Properties.Add(item);
+            }
+            context.TrackEvent(e);
+        }
+        public static void TrackRequest(string name, Uri uri, string code, bool success, long start, long end)
+        {
+            if (context == null) { Initialize(); }
+            if (!enable) { return; }
+            RequestTelemetry request = new RequestTelemetry();
+            request.Name = name;
+            request.ResponseCode = code;
+            request.Success = success;
+            request.Url = uri;
+            request.Timestamp = DateTime.Now;
+            request.Start(start);
+            request.Context.Operation.Name = name;
+            request.Duration = new TimeSpan(end - start);
+            context.TrackRequest(request);
+        }
+
+        public static void TrackRequest(string name, Uri uri, string code, bool success, TimeSpan time)
+        {
+            if (context == null) { Initialize(); }
+            if (!enable) { return; }
+            RequestTelemetry request = new RequestTelemetry();
+            request.Name = name;
+            request.ResponseCode = code;
+            request.Success = success;
+            request.Url = uri;
+            request.Timestamp = DateTime.Now;
+            request.Duration = time;
+            request.Context.Operation.Name = name;
+            context.TrackRequest(request);
+        }
+    }
+}
---
+++ WebhookHelper.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace IntelligentKioskSample
+{
+    class WebhookHelper
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public async static void CallWebhookEndpoint(Dictionary<String, String> payload)
+        {
+            DateTime start = DateTime.Now;
+            Uri endpoint = new Uri(SettingsHelper.Instance.WebhookEndpointURI);
+            StringContent content = new StringContent("{\"results\": [\"" + payload["personname"] + "\"]}", System.Text.Encoding.UTF8, "application/json");
+            String request = await content.ReadAsStringAsync();
+            var response = await httpClient.PostAsync(endpoint, content);
+
+            string responseData = await response.Content.ReadAsStringAsync();
+            if (((int)response.StatusCode) == 200)
+            {
+                System.Diagnostics.Debug.WriteLine("[SUCCESS] HTTP 200 OK");
+                System.Diagnostics.Debug.WriteLine("[VERBOST] RESPONSE:" + responseData);
+                ApplicationInsightsHelper.TrackRequest("Webhook", endpoint, ((int)response.StatusCode).ToString(), true, DateTime.Now - start);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("[ERROR] HTTP {0}", response.StatusCode));
+                ApplicationInsightsHelper.TrackRequest("Webhook", endpoint, ((int)response.StatusCode).ToString(), false, DateTime.Now - start);
+            }
+            System.Diagnostics.Debug.WriteLine(request);
+
+        }
+    }
+}
